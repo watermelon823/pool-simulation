6624fb7c6a272b6dd4858ad3388d4b67462d1503
Anji Ma <amajmgd@gmail.com>
2018-09-26 21:34:55 -0400

    first commit

---
+++ Assets.js
@@ -0,0 +1,51 @@
+let sprites = {};
+let assetsStillLoading = 0;
+
+function assetsLoadingLoop(callback){
+
+	if(assetsStillLoading){
+       requestAnimationFrame(assetsLoadingLoop.bind(this,callback));
+	}
+	else{
+        callback();
+	}
+	
+}
+
+function loadAssets(callback){
+    
+    function loadSprite(fileName){
+    	assetsStillLoading++;
+
+    	let spriteImage = new Image();
+    	spriteImage.src ="./assets/sprites/" + fileName;
+
+    	spriteImage.onload = function(){
+    		assetsStillLoading--;
+    	}
+    	return spriteImage;
+    }
+	sprites.background = loadSprite('spr_background4.png');
+	sprites.stick = loadSprite('spr_stick.png');
+    sprites.whiteBall = loadSprite('spr_ball2.png');
+    sprites.redBall = loadSprite('spr_redball2.png');
+    sprites.yellowBall = loadSprite('spr_yellowball2.png');
+    sprites.blackBall = loadSprite('spr_blackball2.png');
+
+    assetsLoadingLoop(callback);
+}
+
+function getBallSpriteByColor(color){
+
+    switch(color){
+
+        case COLOR.RED:
+             return sprites.redBall;
+        case COLOR.YELLOW:
+             return sprites.yellowBall;
+        case COLOR.BLACK:
+             return sprites.blackBall;
+        case COLOR.WHITE:
+             return sprites.whiteBall;
+    }
+}
---
+++ Ball.js
@@ -0,0 +1,131 @@
+
+const BALL_ORIGIN = new Vector2(25, 25);
+const BALL_DIAMETER = 38;
+const BALL_RADIUS = BALL_DIAMETER/2;
+
+function Ball(position, color){
+	this.position = position;
+	this.velocity = new Vector2();
+	this.moving = false;
+	this.sprite = getBallSpriteByColor(color);
+}
+
+Ball.prototype.update = function(delta){
+    this.position.addTo(this.velocity.mult(delta));
+    
+    //Apply firction
+    this.velocity = this.velocity.mult(0.984);
+
+    if(this.velocity.length()<5){
+    	this.velocity = new Vector2();
+    	this.moving = false;
+    }
+
+
+}
+
+Ball.prototype.draw = function(){
+	Canvas.drawImage(this.sprite, this.position, BALL_ORIGIN);
+}
+
+Ball.prototype.shoot = function(power, rotation){
+
+	this.velocity = new Vector2(power *Math.cos(rotation), power * Math.sin(rotation));
+    this.moving = true;
+}
+
+Ball.prototype.collideWithBall = function(ball){
+	
+	//find a normal vector
+	const n = this.position.subtract(ball.position);
+
+	//find distance
+	const dist = n.length();
+
+	if(dist > BALL_DIAMETER){
+        return;
+	}
+
+	//find minimum translation distance
+	const mtd = n.mult((BALL_DIAMETER - dist) / dist);
+
+	//push-pull balls apart
+	this.position = this.position.add(mtd.mult(1/2));
+	ball.position = ball.position.subtract(mtd.mult(1/2));
+
+	//find unit normal vector
+	const un = n.mult(1/n.length());
+
+	//find unit tangent vector
+	const ut = new Vector2(-un.y, un.x);
+
+	//Project velocity onto the unit normal and unit tangent vectors
+	const v1n = un.dot(this.velocity);
+	const v1t = ut.dot(this.velocity);
+	const v2n = un.dot(ball.velocity);
+	const v2t = ut.dot(ball.velocity);
+
+	//find new normal velocities
+	let v1nTag = v2n;
+	let v2nTag = v1n;
+
+	//conert the scalar normal and tangential velocities into vectors
+	v1nTag = un.mult(v1nTag);
+	const v1tTag = ut.mult(v1t);
+	v2nTag = un.mult(v2nTag);
+	const v2tTag = ut.mult(v2t);
+
+	//update velocities
+	this.velocity = v1nTag.add(v1tTag);
+	ball.velocity = v2nTag.add(v2tTag);
+
+	this.moving = true;
+	ball.moving = true;
+
+}
+
+Ball.prototype.collideWithTable = function(table){
+
+	if(!this.moving){
+		return;
+	}
+
+	let collided = false;
+
+	if(this.position.y <= table.TopY + BALL_RADIUS){
+		this.velocity = new Vector2(this.velocity.x,-this.velocity.y);
+		collided = true;
+	}
+
+	if(this.position.x >= table.RightX - BALL_RADIUS){
+		this.velocity = new Vector2(-this.velocity.x,this.velocity.y);
+		collided = true;
+	}
+
+	if(this.position.y >= table.BottomY - BALL_RADIUS){
+		this.velocity = new Vector2(this.velocity.x,-this.velocity.y);
+		collided = true;
+	}
+
+	if(this.position.x <= table.LeftX + BALL_RADIUS){
+		this.velocity = new Vector2(-this.velocity.x,this.velocity.y);
+		collided = true;
+	}
+
+	if(collided){
+		this.velocity = this.velocity.mult(0.98);
+	}
+
+	
+}
+
+Ball.prototype.collideWith = function(object){
+	if(object instanceof Ball){
+		this.collideWithBall(object);
+	}
+	else{
+       this.collideWithTable(object);
+	}
+
+	
+}
